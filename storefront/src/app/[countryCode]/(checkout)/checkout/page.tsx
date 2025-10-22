2451b621da14246bb1e2ade3f700883a34bab8cf
valentin BRONFORT <welcome@mendly.io>
2025-10-22 13:01:16 +0400

    fix: utilisation de --no-frozen-lockfile pour Railway

--- storefront/src/app/[countryCode]/(checkout)/checkout/page.tsx
+++ storefront/src/app/[countryCode]/(checkout)/checkout/page.tsx
@@ -111,9 +111,9 @@ export default async function Checkout() {
               </div>
 
               <div className="p-6 md:p-8">
-                <Wrapper cart={cart}>
-                  <CheckoutForm cart={cart} customer={customer} />
-                </Wrapper>
+            <Wrapper cart={cart}>
+              <CheckoutForm cart={cart} customer={customer} />
+            </Wrapper>
               </div>
             </div>
 
@@ -145,24 +145,24 @@ export default async function Checkout() {
                 <li className="flex items-center gap-2">
                   <div className="w-5 h-5 bg-green-100 rounded-full flex items-center justify-center flex-shrink-0">
                     <svg className="w-3 h-3 text-green-600" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                    </svg>
+                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                  </svg>
                   </div>
                   <span>Cryptage SSL 256 bits</span>
                 </li>
                 <li className="flex items-center gap-2">
                   <div className="w-5 h-5 bg-green-100 rounded-full flex items-center justify-center flex-shrink-0">
                     <svg className="w-3 h-3 text-green-600" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                    </svg>
+                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                  </svg>
                   </div>
                   <span>Vos données ne sont jamais stockées</span>
                 </li>
                 <li className="flex items-center gap-2">
                   <div className="w-5 h-5 bg-green-100 rounded-full flex items-center justify-center flex-shrink-0">
                     <svg className="w-3 h-3 text-green-600" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                    </svg>
+                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                  </svg>
                   </div>
                   <span>Conforme PCI-DSS</span>
                 </li>
