e6e0b9e5c2f17c3051541d89765f186b95bad7c0
valentin BRONFORT <welcome@mendly.io>
2025-11-14 13:30:01 +0400

    fix: nom module cohérent stock_alerts partout (underscore)

--- backend/src/modules/stock-alerts/index.ts
+++ backend/src/modules/stock-alerts/index.ts
@@ -1,7 +1,7 @@
 import { Module } from "@medusajs/framework/utils"
 import StockAlertService from "./service"
 
-export const STOCK_ALERT_MODULE = "stock-alert"
+export const STOCK_ALERT_MODULE = "stock_alerts"
 
 export default Module(STOCK_ALERT_MODULE, {
   service: StockAlertService,
