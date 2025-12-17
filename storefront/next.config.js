ceca2fcc8f2705e5f4b23116e290e18e4c2fd20b
valentin BRONFORT <welcome@mendly.io>
2025-12-17 19:20:35 +0100

    fix: Simplification config remotePatterns Next.js 15
    - Suppression des variables d'env undefined qui causaient des hostnames invalides
    - Simplification en gardant uniquement les hostnames nécessaires
    - Le bucket Railway est maintenant hardcodé (pas de variable d'env)

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -18,15 +18,10 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
-        
       },
-      { // Note: needed to serve images from /public folder
-        protocol: process.env.NEXT_PUBLIC_BASE_URL?.startsWith('https') ? 'https' : 'http',
-        hostname: process.env.NEXT_PUBLIC_BASE_URL?.replace(/^https?:\/\//, ''),
-      },
-      { // Note: only needed when using local-file for product media
+      {
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL?.replace('https://', ''),
+        hostname: "localhost",
       },
       { // Note: can be removed after deleting demo products
         protocol: "https",
@@ -40,14 +35,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
-      { // Railway bucket storage
+      { // Railway MinIO bucket - OBLIGATOIRE pour les images
         protocol: "https",
         hostname: "bucket-production-de72.up.railway.app",
       },
-      ...(process.env.NEXT_PUBLIC_MINIO_ENDPOINT ? [{ // Note: needed when using MinIO bucket storage for media
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_MINIO_ENDPOINT,
-      }] : []),
     ],
   },
 }
